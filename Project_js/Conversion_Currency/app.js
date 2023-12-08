59373124731648baf0befc36e72a92f56627e323
ekthanutp <ekthanut.p@hotmail.com>
2023-12-08 12:34:17 +0700

    tt

--- Project_js/Conversion_Currency/app.js
+++ Project_js/Conversion_Currency/app.js
@@ -1,4 +1,5 @@
 const currency_one = document.getElementById('currency_one');
 const currency_two = document.getElementById('currency_two');
-const amount_one = document.getElementById('amount-one')
-const amount_two = document.getElementById('amount-two')
+const amount_one = document.getElementById('amount-one');
+const amount_two = document.getElementById('amount-two');
+const rateText = document.getElementById('rate')
